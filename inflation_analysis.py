b44518a62cdfbaab7c53742d5832c70d4b108a81
Valtepl1002 <valeriateplu2020@gmail.com>
2025-11-07 14:45:51 +0200

    inflation_analysis.py

--- inflation_analysis.py
+++ inflation_analysis.py
@@ -9,7 +9,7 @@ import os
 x = np.linspace(1, 10, 500)
 
 # Обчислення значень функції
-y = 5 * np.sin(x) * (np.cos(x**2 + 1/x))**2
+y = 5 * np.sin(x) * (np.cos(x2 + 1/x))2
 
 # Побудова графіка
 fig, ax = plt.subplots(figsize=(8, 5))
@@ -18,7 +18,7 @@ ax.plot(x, y, linestyle='-', linewidth=2, color='blue',
 
 ax.set_xlabel('x')
 ax.set_ylabel('Y(x)')
-ax.set_title('Графік функції Y(x) = 5*sin(x) * cos(x^2 + 1/x)^2')
+ax.set_title('Графік функції Y(x) = 5*sin(x) * cos^2(x^2 + 1/x)')
 ax.legend()
 ax.grid(True)
 
